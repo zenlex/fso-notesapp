ab88b99bfd2a29275583b1e763ddbdb009d71340
zenlex <erichrkeil@gmail.com>
2022-01-25 20:27:52 -0600

    basic mongo setup
    - add mongo and mongoose deps
    - add db url and cli for auth
    - code blocks for creating note and querying notes

---
+++ backend/mongo.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+if (process.argv.length < 3) {
+  console.log('Please provide the password as an argument: node mongo.js <password>');
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://zenlex:${password}@cluster0.oofub.mongodb.net/fso-notesapp?retryWrites=true&w=majority`;
+
+mongoose.connect(url);
+
+const noteSchema = new Schema({
+  content: String, 
+  date: Date,
+  important: Boolean,
+})
+
+const Note = mongoose.model('Note', noteSchema)
+
+// const note = new Note({
+//   content: 'HTML is Easy',
+//   date: new Date(),
+//   important: true
+// })
+
+// note.save().then(result => {
+//   console.log('note saved')
+//   mongoose.connection.close()
+// })
+
+Note.find({}).then(result => {
+  result.forEach(note => {
+    console.log(note)
+  })
+  mongoose.connection.close();
+})
