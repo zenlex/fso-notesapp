3daf826b4faddb253b504ce0a2f13ad4fc3f5641
zenlex <erichrkeil@gmail.com>
2022-01-28 20:19:20 -0600

    Integrate MongoDB
    - moved functionality for CRUD operations to mongoose models
    - added todo file for bookmarking WIP
    - added eslint config

--- backend/index.js
+++ backend/index.js
@@ -11,34 +11,31 @@ app.use(express.json())
 app.use(cors());
 app.use(express.static('build'))
 
-const generateId = () => {
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => n.id))
-    : 0
-  return maxId + 1
-}
+// const generateId = (notes) => {
+//   const maxId = notes.length > 0
+//     ? Math.max(...notes.map(n => n.id))
+//     : 0
+//   return maxId + 1
+// }
 
 // ROUTES
 app.post('/api/notes', (req, res) => {
 
   const body = req.body;
 
-  if(!body.content) {
+  if (body.content === undefined) {
     return res.status(400).json({
       error: 'content missing'
     })
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
     date: new Date(),
-    id: generateId()
-  }
-  
-  notes = notes.concat(note)
+  })
 
-  res.json(note);
+  note.save().then(savedNote => res.json(savedNote))
 })
 
 app.get('/', (req, res) => {
@@ -49,29 +46,18 @@ app.get('/api/notes', (req, res) => {
   Note.find({}).then(notes => {
     res.json(notes)
   })
-  // res.json(notes);
 });
 
 app.get('/api/notes/:id', (req, res) => {
-  const id = Number(req.params.id)
-  console.log({ id })
-  const note = notes.find(note => {
-    console.log(note.id, typeof note.id, id, typeof id, note.id === id);
-    return note.id === id
+  Note.findById(req.params.id).then(note => {
+    res.json(note)
   })
-  console.log(note);
-  if (note) {
-    res.json(note);
-  } else {
-    res.status(404).end()
-  }
 })
 
 app.delete('/api/notes/:id', (req, res) => {
-  const id = Number(req.params.id)
-  notes = notes.filter(note => note.id !== id);
-
-  res.status(204).end();
+  Note.findByIdAndDelete(req.params.id).then(() => {
+    res.status(204).end();
+  })
 })
 
 // REQUEST LISTENER
