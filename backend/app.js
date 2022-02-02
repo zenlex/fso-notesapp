77df13c7edbc52181d198daeeff1a43e3d34de29
Erich Keil <39918654+zenlex@users.noreply.github.com>
2022-02-01 22:20:07 -0600

    Backendrestruct (#4)
    * Restructure Components to Modules
    
    - create router app
    - create logger js
    - create middleware util
    - create notes controller

---
+++ backend/app.js
@@ -0,0 +1,31 @@
+// IMPORTS
+const config = require('./utils/config')
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const notesRouter = require('./controllers/notes')
+const middleware = require('./utils/middleware')
+const logger = require('./utils/logger')
+const mongoose = require('mongoose')
+
+console.log('connecting to MongoDB');
+
+mongoose.connect(config.MONGODB_URI)
+  .then((result) => {
+    logger.info('connected to MongoDB', result.connections[0].host);
+  })
+  .catch((err) => {
+    logger.info('error connecting to MongoDB: ', err.message)
+  })
+
+app.use(cors());
+app.use(express.static('build'))
+app.use(express.json())
+app.use(middleware.requestLogger)
+
+app.use('/api/notes', notesRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
--- backend/models/note.js
+++ backend/models/note.js
@@ -1,37 +1,25 @@
 const mongoose = require('mongoose')
-const {Schema} = mongoose
+const { Schema } = mongoose
 
-const url = process.env.MONGO_URL;
+const noteSchema = new Schema({
+  content: {
+    type: String,
+    minLength: 5,
+    required: true
+  },
+  date: {
+    type: Date,
+    required: true
+  },
+  important: Boolean,
+});
 
-console.log('connecting to MongoDB');
+noteSchema.set('toJSON', {
+  transform: (document, returnedObj) => {
+    returnedObj.id = returnedObj._id.toString()
+    delete returnedObj._id
+    delete returnedObj.__v
+  }
+})
 
-mongoose.connect(url)
-  .then((result) => {
-    console.log('connected to MongoDB', result.connections[0].host);
-  })
-  .catch((err) => {
-    console.log('error connecting to MongoDB: ', err.message)
-  })
-
-  const noteSchema = new Schema({
-    content: {
-      type:String, 
-      minLength: 5,
-      required: true
-    },
-    date: {
-      type: Date,
-      required: true
-    },
-    important: Boolean,
-  });
-  
-  noteSchema.set('toJSON', {
-    transform: (document, returnedObj) => {
-      returnedObj.id = returnedObj._id.toString()
-      delete returnedObj._id
-      delete returnedObj.__v
-    }
-  })
-
-  module.exports = mongoose.model('Note', noteSchema);
+module.exports = mongoose.model('Note', noteSchema);
