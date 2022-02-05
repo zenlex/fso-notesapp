30ce1b8cf5b64b4cd2f8091b1d31eec5efb552b3
zenlex <erichrkeil@gmail.com>
2022-02-05 17:39:56 -0600

    convert error handling to express-async-errors

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,13 @@
 // IMPORTS
-const config = require('./utils/config')
+const config = require('./utils/config');
 const express = require('express');
+require('express-async-errors');
 const app = express();
 const cors = require('cors');
-const notesRouter = require('./controllers/notes')
-const middleware = require('./utils/middleware')
-const logger = require('./utils/logger')
-const mongoose = require('mongoose')
+const notesRouter = require('./controllers/notes');
+const middleware = require('./utils/middleware');
+const logger = require('./utils/logger');
+const mongoose = require('mongoose');
 
 console.log('connecting to MongoDB');
 
@@ -15,17 +16,17 @@ mongoose.connect(config.MONGODB_URI)
     logger.info('connected to MongoDB', result.connections[0].host);
   })
   .catch((err) => {
-    logger.info('error connecting to MongoDB: ', err.message)
-  })
+    logger.info('error connecting to MongoDB: ', err.message);
+  });
 
 app.use(cors());
-app.use(express.static('build'))
-app.use(express.json())
-app.use(middleware.requestLogger)
+app.use(express.static('build'));
+app.use(express.json());
+app.use(middleware.requestLogger);
 
-app.use('/api/notes', notesRouter)
+app.use('/api/notes', notesRouter);
 
-app.use(middleware.unknownEndpoint)
-app.use(middleware.errorHandler)
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
 
-module.exports = app
+module.exports = app;
--- backend/controllers/notes.js
+++ backend/controllers/notes.js
@@ -3,31 +3,22 @@ const Note = require('../models/note');
 
 
 // ROUTES
-notesRouter.get('/', async (req, res, next) => {
-  try {
+notesRouter.get('/', async (req, res) => {
     const notes = await Note.find({});
     res.json(notes);
-  } catch (err) {
-    next(err);
-  }
 });
 
-notesRouter.get('/:id', async (req, res, next) => {
+notesRouter.get('/:id', async (req, res) => {
   const note = await Note.findById(req.params.id);
-  try {
     if (note) {
       res.json(note);
     } else {
       res.status(404).end();
     }
-  } catch (err) {
-    next(err);
-  }
 });
 
-notesRouter.post('/', async (req, res, next) => {
+notesRouter.post('/', async (req, res) => {
   const body = req.body;
-  try {
     const note = new Note({
       content: body.content,
       important: body.important || false,
@@ -36,32 +27,21 @@ notesRouter.post('/', async (req, res, next) => {
 
     const savedNote = await note.save();
     res.status(201).json(savedNote.toJSON());
-  } catch (err) {
-    next(err);
-  }
 });
 
-notesRouter.delete('/:id', async (req, res, next) => {
-  try {
+notesRouter.delete('/:id', async (req, res) => {
     await Note.findByIdAndDelete(req.params.id);
     res.status(204).end();
-  } catch (err) {
-    next(err);
-  }
 });
 
-notesRouter.put('/:id', async (req, res, next) => {
+notesRouter.put('/:id', async (req, res) => {
   const originalNote = {
     content: req.body.content,
     important: req.body.important
   };
-  try {
     const updatedNote = await Note
       .findByIdAndUpdate(req.params.id, originalNote, { new: true });
     res.json(updatedNote);
-  }catch(err){
-    next(err);
-  }
 });
 
 module.exports = notesRouter;
