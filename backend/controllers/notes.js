3d5099e0452f1d5c1a596fb130f04899fc5af1e6
zenlex <erichrkeil@gmail.com>
2022-02-08 21:42:52 -0600

    note user references

--- backend/controllers/notes.js
+++ backend/controllers/notes.js
@@ -1,5 +1,6 @@
 const notesRouter = require('express').Router();
 const Note = require('../models/note');
+const User = require('../models/user');
 
 
 // ROUTES
@@ -19,14 +20,20 @@ notesRouter.get('/:id', async (req, res) => {
 
 notesRouter.post('/', async (req, res) => {
   const body = req.body;
+
+  const user = await User.findById(body.userId);
+  
   const note = new Note({
     content: body.content,
     important: body.important || false,
     date: new Date(),
+    user: user._id
   });
 
   const savedNote = await note.save();
-  res.status(201).json(savedNote.toJSON());
+  user.notes = user.notes.concat(savedNote._id);
+  await user.save();
+  res.status(201).json(savedNote);
 });
 
 notesRouter.delete('/:id', async (req, res) => {
