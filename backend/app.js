613a31ce93905008a0bf6f65518c2e33d6823f2a
zenlex <erichrkeil@gmail.com>
2022-02-08 21:00:33 -0600

    add basic user validation

--- backend/app.js
+++ backend/app.js
@@ -5,9 +5,11 @@ require('express-async-errors');
 const app = express();
 const cors = require('cors');
 const notesRouter = require('./controllers/notes');
+const usersRouter = require('./controllers/users');
 const middleware = require('./utils/middleware');
 const logger = require('./utils/logger');
 const mongoose = require('mongoose');
+
 
 console.log('connecting to MongoDB');
 
@@ -24,6 +26,7 @@ app.use(express.static('build'));
 app.use(express.json());
 app.use(middleware.requestLogger);
 
+app.use('/api/users', usersRouter);
 app.use('/api/notes', notesRouter);
 
 app.use(middleware.unknownEndpoint);
--- backend/models/user.js
+++ backend/models/user.js
@@ -1,8 +1,13 @@
 const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
 const {Schema} = mongoose;
 
 const userSchema = new Schema({
-  username: String,
+  // TODO Add additional validation to user creation
+  username: {
+    type: String,
+    unique: true
+  },
   name: String,
   passwordHash: String,
   notes: [
@@ -13,16 +18,19 @@ const userSchema = new Schema({
   ],
 });
 
+userSchema.plugin(uniqueValidator);
 
 userSchema.set('toJSON', {
   transform: (document, returnedObject) => {
     returnedObject.id = returnedObject._id.toString();
     delete returnedObject._id;
-    delete returnedObject._v;
+    delete returnedObject.__v;
     // DO NOT RETURN PASSWORD HASH
     delete returnedObject.passwordHash;
   }
 });
+
+userSchema.plugin(uniqueValidator);
 
 const User = mongoose.model('User', userSchema);
 
