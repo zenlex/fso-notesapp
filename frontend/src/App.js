7aca3fc3ac90a69dfb3e83de1ea3110f69145aa0
zenlex <erichrkeil@gmail.com>
2022-02-12 17:41:24 -0600

    refactor footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,20 +2,7 @@ import React, { useState, useEffect } from "react";
 import Note from './components/Note'
 import Notification from './components/Notification'
 import noteService from './services/notes';
-
-const Footer = () => {
-  const footerStyle = {
-    color: 'green', 
-    fontStyle: 'italic', 
-    fontSize: 16
-  }
-  return (
-    <div style={footerStyle}>
-      <br />
-      <em>Note app, Department of Computer Science, University of Helsinki 2021</em>
-    </div>
-  )
-}
+import Footer from './components/Footer'
 
 const App = () => {
   const [notes, setNotes] = useState([]);
